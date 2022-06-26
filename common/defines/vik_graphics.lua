ab4a838344818c4dce31fc2817bef05f71da758d
prolox-ita <lorenzo.provenzano17@gmail.com>
2022-06-26 19:57:45 +0200

    Regnum Italicum done
    + Made states of Regnum Italicum
    --> TODO: finish the states on the balkan area, needs the VPs, population and buildings
    + Recalculated requirements for state progression
    ++ So i had to re-assign the category to all the previous states; for now I did Cordoba, needed to be done Asturia, France and England

---
+++ common/defines/vik_graphics.lua
@@ -0,0 +1,3 @@
+-- REWORKED mod
+NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {300, 500, 700}  -- At what camera distance the VP name text disappears. #150;250;500 #small;medium (circle); capital
+NDefines_Graphics.NGraphics.VICTORY_POINTS_DISTANCE_CUTOFF = {400, 500, 700} -- At what distance VPs are hidden

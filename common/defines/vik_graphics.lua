dde4d3c2b896bce18899c57eeadeb15f90225b48
prolox-ita <lorenzo.provenzano17@gmail.com>
2022-07-10 01:49:22 +0200

    Changes before reset of PC

--- common/defines/vik_graphics.lua
+++ common/defines/vik_graphics.lua
@@ -1,3 +1,5 @@
 -- REWORKED mod
 NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {300, 500, 700}  -- At what camera distance the VP name text disappears. #150;250;500 #small;medium (circle); capital
 NDefines_Graphics.NGraphics.VICTORY_POINTS_DISTANCE_CUTOFF = {400, 500, 700} -- At what distance VPs are hidden
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_TEX_WIDTH = 104 -- Expected texture size
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_TEX_HEIGHT = 100
